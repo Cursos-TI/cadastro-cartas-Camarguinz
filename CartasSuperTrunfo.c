102df9394e07777fdf8fb5be77b29ad13763e189
Pedro Camargo <122657413+Camarguinz@users.noreply.github.com>
2025-10-05 04:20:31 +0000

    Inserção do código do VScode

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,9 +5,6 @@
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
 int main() {
-  // Área para definição das variáveis para armazenar as propriedades das cidades
-  // Força o console do Windows a usar UTF-8
-    SetConsoleOutputCP(CP_UTF8);
 
     
     // Declaração das variáveis para as duas cartas
